aa53e8fe968d42fd8e2f21ea10446e6f4ce5dbdd
amrocs <amrocs@yahoo.co.jp>
2016-10-13 14:44:50 +0900

    css loading updated

--- cssloading_demo/cssLoading.js
+++ cssloading_demo/cssLoading.js
@@ -6,6 +6,8 @@
  *      jQueryElement.cssLoading('show', 'small');
  *      jQueryElement.cssLoading('show', 'large');
  *      jQueryElement.cssLoading('hide');
+ *      jQueryElement.cssLoading('showMask'); // without loading animation
+ *      jQueryElement.cssLoading('hideMask'); 
  * 
  *    Caution:
  *      jQueryElement's position property of style is set 'relative'.
@@ -14,19 +16,19 @@
  *      http://www.alessioatzeni.com/wp-content/tutorials/html-css/CSS3-loading-animation-loop/index.html
 */
 'use strict';
-(function($){
+(function($) {
   var methods = {
-    init: function($el, size){
+    init: function($el, size) {
       $el.css('position', 'relative');
       var d = {
         $loading: $("<div/>").addClass('cssloading').appendTo($el),
         $mask: $("<div/>").addClass('cssloading_mask').appendTo($el)
       };
       
-      if(size){
-        if(size.toLowerCase() === 'small'){
+      if (size) {
+        if (size.toLowerCase() === 'small') {
           d.$loading.addClass('cssloading_small')
-        } else if (size.toLowerCase() === 'large'){
+        } else if (size.toLowerCase() === 'large') {
           d.$loading.addClass('cssloading_large')
         }
       }
@@ -35,32 +37,58 @@
       
       return $el;
     },
-    show: function(size){
+    show: function(size) {
       var d = this.data('cssLoading');
-      if(!d){
+      if (!d) {
         methods.init(this, size);
         d = this.data('cssLoading');
       }
-      d.$loading.show();
-      d.$mask.show();
-      
+      if (d) {
+        d.$loading.show();
+        d.$mask.show();
+      }
       return this;
     },
-    hide: function(){
+    hide: function() {
       var d = this.data('cssLoading');
-      if(!d){
+      if (!d) {
         var $el = methods.init(this);
         d = $el.data('cssLoading');
       }
-      d.$loading.hide();
-      d.$mask.hide();
-      
+      if (d) {
+        d.$loading.hide();
+        d.$mask.hide();
+      }
+      return this;
+    },
+    showMask: function() {
+      var d = this.data('cssLoading');
+      if (!d) {
+        methods.init(this);
+        d = this.data('cssLoading');
+      }
+      if (d) {
+        d.$loading.hide();
+        d.$mask.show();
+      }
+      return this;
+    },
+    hideMask: function() {
+      var d = this.data('cssLoading');
+      if (!d) {
+        var $el = methods.init(this);
+        d = $el.data('cssLoading');
+      }
+      if (d) {
+        d.$loading.hide();
+        d.$mask.hide();
+      }
       return this;
     },
   };
   
   $.fn.cssLoading = function(method, size) {
-    if( methods[method] ) {
+    if ( methods[method] ) {
       return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
     } else if ( typeof method === 'object' || ! method ) {
       return methods.init.apply( this, arguments ); // Default to "init"
